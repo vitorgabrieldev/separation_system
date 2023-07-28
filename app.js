88ee269694bbb0719c1ba6515715a8e210397cee
vitorgabrieldev <vitorgabrieldeoliveiradev@gmail.com>
2023-07-28 09:02:23 -0300

    Done project

--- app.js
+++ app.js
@@ -3,24 +3,43 @@ var itens_name = [];
 var itens_qtd = [];
 var itens_value = [];
 
-document.querySelector('#btnAdd').addEventListener('click', () => {
+document.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') main();
+});
+
+document.querySelector('#btnAdd').addEventListener('click', () => main());
+
+const main = () => {
     var name = $('#itemName').val();
-    var qtd = $('#itemQtd').val();
+    var qtd;
     var value;    
+    
     if ($('#itemValue').val()) {
         value = $('#itemValue').val();
     } else {
         value = '00,00';
     };
 
-    if(name&&qtd) {
+    if ($('#itemQtd').val()) {
+        qtd = $('#itemQtd').val();
+    } else {
+        qtd = '1';
+    };
+
+    if(name) {
         save({name:name,qtd:qtd,value:value});
         render({name:name,qtd:qtd,value:value});      
         document.querySelector('#itemName').value = "";
         document.querySelector('#itemQtd').value = "";
         document.querySelector('#itemValue').value = "";
-    };  
-});
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Prencha o nome do produto!',
+        });
+    };
+};
 
 const render = (data) => {
     document.querySelector('#boardList').innerHTML = "";
@@ -38,6 +57,12 @@ const render = (data) => {
         </tr>
         `;  
     });
+    countItens(itens_name.length);
+
+    let total = 0;
+    itens_value.forEach(element => total += parseInt(element));
+    countValue(total);
+
 };
 
 const save = (data) => {
@@ -58,6 +83,10 @@ const editRow = (i) => {
     document.querySelector('#itemName').focus();
     document.querySelector('#itemQtd').value = itens_qtd[(i - 1)];
     document.querySelector('#itemValue').value = itens_value[(i - 1)];
+
+    itens_name.splice(itens_name[(i - 1), 1]);
+    itens_qtd.splice(itens_qtd[(i - 1), 1]);
+    itens_value.splice(itens_value[(i - 1), 1]);
 };
 const maxLength = 15;
 
@@ -66,4 +95,12 @@ const truncateString = (str) => {
         return str.slice(0, maxLength) + '...';
     };
     return str;
+};
+
+const countItens = (i) => {
+    $('#qtdItens').text(`Itens : ${i}`);
+};
+
+const countValue = (i) => {
+    $('#totalItens').text(`Total : ${i}`);
 };
